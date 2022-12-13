23b612dd4aa05838b77e4014b4cf296c60488360
Linmingdao <mr.lmd@outlook.com>
2022-12-13 20:39:25 +0800

    条件类型

--- 06-type-manipulation/src/10-conditional-types.ts
+++ 06-type-manipulation/src/10-conditional-types.ts
@@ -1,44 +1,45 @@
-/* // interface Animal {
-//   live(): void
-// }
+// 条件类型：case 1
+interface Animal {
+  live(): void;
+}
 
-// interface Dog extends Animal {
-//   woof(): void
-// }
+interface Dog extends Animal {
+  woof(): void;
+}
 
-// // type Example1 = number
-// type Example1 = Dog extends Animal ? number : string
+type Example1 = Dog extends Animal ? number : string; // type Example1 = number;
+type Example2 = RegExp extends Animal ? number : string; // type Example2 = string;
 
-// // type Example2 = string
-// type Example2 = RegExp extends Animal ? number : string
-
-
+// 没有使用条件类型：case 2
 interface IdLabel {
-  id: number
+  id: number;
 }
+
 interface NameLabel {
-  name: string
+  name: string;
 }
 
-// function createLabel(id: number): IdLabel
-// function createLabel(name: string): NameLabel
-// function createLabel(nameOrId: string | number): IdLabel | NameLabel
-// function createLabel(nameOrId: string | number): IdLabel | NameLabel {
-//   throw ''
-// }
+function createLabel(id: number): IdLabel;
+function createLabel(name: string): NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel {
+  throw '';
+}
 
+// 使用条件类型：case 3
+type NameOrId<T extends number | string> = T extends number
+  ? IdLabel
+  : NameLabel;
 
-type NameOrId<T extends number | string> = T extends number ? IdLabel : NameLabel
-
-function createLabel<T extends number | string>(idOrName: T): NameOrId<T> {
-  throw ''
+function createLabel2<T extends number | string>(idOrName: T): NameOrId<T> {
+  throw '';
 }
 
 // type a = NameLabel
-let a = createLabel('typescript')
+let v = createLabel2('typescript');
 
 // type b = IdLabel
-let b = createLabel(2.8)
+let b = createLabel2(2.8);
 
 // type c = NameLabel | IdLabel
-let c = createLabel(Math.random() > 0.5 ? 'hello' : 42) */
+let c = createLabel2(Math.random() > 0.5 ? 'hello' : 42);
